11fe38e2239a06a1f99c825cf3abd664f976f188
rkgupta1298 <rkgupta1298@gmail.com>
2019-02-28 11:06:04 +0530

    SignUpIntegrated

--- apimedic_Module-master/MediFinder-PatiendEnd/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ apimedic_Module-master/MediFinder-PatiendEnd/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f080023;
-        public static final int end = 0x7f08005c;
-        public static final int gone = 0x7f080063;
-        public static final int invisible = 0x7f08007e;
-        public static final int left = 0x7f080080;
-        public static final int packed = 0x7f08009b;
-        public static final int parent = 0x7f08009c;
-        public static final int percent = 0x7f08009e;
-        public static final int right = 0x7f0800b4;
-        public static final int spread = 0x7f0800dc;
-        public static final int spread_inside = 0x7f0800dd;
-        public static final int start = 0x7f0800e2;
-        public static final int top = 0x7f0800fe;
-        public static final int wrap = 0x7f080107;
+        public static final int end = 0x7f08005d;
+        public static final int gone = 0x7f080064;
+        public static final int invisible = 0x7f080080;
+        public static final int left = 0x7f080082;
+        public static final int packed = 0x7f08009e;
+        public static final int parent = 0x7f08009f;
+        public static final int percent = 0x7f0800a3;
+        public static final int right = 0x7f0800be;
+        public static final int spread = 0x7f0800e7;
+        public static final int spread_inside = 0x7f0800e8;
+        public static final int start = 0x7f0800ed;
+        public static final int top = 0x7f080109;
+        public static final int wrap = 0x7f080113;
     }
     public static final class styleable {
         private styleable() {}
